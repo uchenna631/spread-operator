14e9a8fabb3fc37ea6cc09428f4dcc73475459a2
Fimber Uchenna Eze <uchenna631@gmail.com>
2022-05-24 20:49:03 +0000

    Spread operator

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -4,9 +4,15 @@
  */
 
 // No spread operator
-
+let arr1 = [2, 3, 4]
 // Copying an array
-
+let arr2 = [...arr1]
+arr2.push(5)
 // Copying an object
-
+let arr3 = {a:6, b:7}
 // Copying only part of an array/object
+let arr4 = [...arr1, 9, 10, 11, {...arr3}]
+console.log('First Array:', arr1)
+console.log('Second Array:', arr2)
+console.log('Third Array:', arr3)
+console.log('Fourth Array:', arr4)
